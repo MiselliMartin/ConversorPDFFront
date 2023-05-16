a553d5a8a6efcb56b00e63762ecdfa6018c1c233
Martín Miselli <martin.miselli@gmail.com>
2023-05-16 11:55:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ document.getElementById("uploadForm").addEventListener("submit", async function(
     console.log("Datos del formulario:", Object.fromEntries(formData));
     
     try {
-        const response = await fetch("https://conversorpdf.onrender.com/convert", {
+        const response = await fetch("MiselliMartin.pythonanywhere.com/convert", {
           method: "POST",
           body: formData,
           mode: "cors",
