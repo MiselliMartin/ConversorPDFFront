2afd9fd22405ad1fde634ddd2dba49f6cf9d7aa4
Martín Miselli <martin.miselli@gmail.com>
2023-05-22 09:48:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ document.getElementById("uploadForm").addEventListener("submit", async function(
     console.log("Datos del formulario:", Object.fromEntries(formData));
  
     try {
-        const response = await fetch("http://127.0.0.1:5000/convert", {
+        const response = await fetch("https://conversorpdf.onrender.com/convert", {
           method: "POST",
           body: formData,
           mode: "cors",
